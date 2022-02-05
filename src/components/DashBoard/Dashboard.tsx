4be6744ef54f9bb2d24d853116fc068006fb0972
Brax-1 <ay701548@gmail.com>
2022-02-06 01:04:51 +0530

    Sorting feature added

--- src/components/DashBoard/Dashboard.tsx
+++ src/components/DashBoard/Dashboard.tsx
@@ -44,6 +44,55 @@ const Dashboard = () => {
   }
   const [data, setData] = useState<ApiInnerData[]>([])
 
+  async function SortingData(value: string) {
+    let sortedData = data
+    if (value === 'date') {
+      sortedData = await Promise.all(
+        data.sort((x, y) => (x.date < y.date ? -1 : 1))
+      )
+    } else if (value === 'name') {
+      sortedData = await Promise.all(
+        data.sort((x, y) => (x.name < y.name ? -1 : 1))
+      )
+    } else if (value === 'activity_name') {
+      sortedData = await Promise.all(
+        data.sort((x, y) => (x.activity_name < y.activity_name ? -1 : 1))
+      )
+    } else if (value === 'score') {
+      sortedData = await Promise.all(
+        data.sort((x, y) => (x.score < y.score ? -1 : 1))
+      )
+    } else if (value === 'attempt_type') {
+      sortedData = await Promise.all(
+        data.sort((x, y) => (x.attempt_type < y.attempt_type ? -1 : 1))
+      )
+    } else if (value === 'attemp_numer') {
+      sortedData = await Promise.all(
+        data.sort((x, y) => (x.attemp_numer < y.attemp_numer ? -1 : 1))
+      )
+    } else if (value === 'level') {
+      sortedData = await Promise.all(
+        data.sort((x, y) => (x.level < y.level ? -1 : 1))
+      )
+    } else if (value === 'level_attempt_type') {
+      sortedData = await Promise.all(
+        data.sort((x, y) =>
+          x.level_attempt_type < y.level_attempt_type ? -1 : 1
+        )
+      )
+    } else if (value === 'leaderboard_rank') {
+      sortedData = await Promise.all(
+        data.sort((x, y) => (x.leaderboard_rank < y.leaderboard_rank ? -1 : 1))
+      )
+    } else if (value === 'consistency') {
+      sortedData = await Promise.all(
+        data.sort((x, y) => (x.consistency < y.consistency ? -1 : 1))
+      )
+    }
+
+    setData(sortedData)
+  }
+
   async function getData() {
     const data = ((await QuizApi.tableData()) as unknown) as ApiInnerData[][]
     setData(data[0])
@@ -201,17 +250,72 @@ const Dashboard = () => {
                 <table cellSpacing="0">
                   <thead>
                     <tr>
-                      <th style={{ width: '4%' }}>Serial No</th>
-                      <th style={{ width: '7%' }}>Date</th>
-                      <th style={{ width: '13%' }}>S Name</th>
-                      <th style={{ width: '13%' }}>Activity Name</th>
-                      <th style={{ width: '5%' }}>Score</th>
-                      <th style={{ width: '7%' }}>Attempt Type</th>
-                      <th style={{ width: '6%' }}>Attempt Number</th>
-                      <th style={{ width: '6%' }}>Level</th>
-                      <th style={{ width: '9%' }}>Level Activity</th>
-                      <th style={{ width: '7%' }}>Leaderboard Rank</th>
-                      <th style={{ width: '5%' }}>Consistency</th>
+                      <th
+                        onClick={() => SortingData('serial')}
+                        style={{ width: '4%' }}
+                      >
+                        Serial No <i className="fas fa-sort"></i>
+                      </th>
+                      <th
+                        onClick={() => SortingData('date')}
+                        style={{ width: '7%' }}
+                      >
+                        Date <i className="fas fa-sort"></i>
+                      </th>
+                      <th
+                        onClick={() => SortingData('name')}
+                        style={{ width: '13%' }}
+                      >
+                        S Name <i className="fas fa-sort"></i>
+                      </th>
+                      <th
+                        onClick={() => SortingData('activity_name')}
+                        style={{ width: '13%' }}
+                      >
+                        Activity Name <i className="fas fa-sort"></i>
+                      </th>
+                      <th
+                        onClick={() => SortingData('score')}
+                        style={{ width: '5%' }}
+                      >
+                        Score <i className="fas fa-sort"></i>
+                      </th>
+                      <th
+                        onClick={() => SortingData('attempt_type')}
+                        style={{ width: '7%' }}
+                      >
+                        Attempt Type <i className="fas fa-sort"></i>
+                      </th>
+                      <th
+                        onClick={() => SortingData('attemp_numer')}
+                        style={{ width: '6%' }}
+                      >
+                        Attempt Number <i className="fas fa-sort"></i>
+                      </th>
+                      <th
+                        onClick={() => SortingData('level')}
+                        style={{ width: '6%' }}
+                      >
+                        Level <i className="fas fa-sort"></i>
+                      </th>
+                      <th
+                        onClick={() => SortingData('level_attempt_type')}
+                        style={{ width: '9%' }}
+                      >
+                        Level Activity <i className="fas fa-sort"></i>
+                      </th>
+                      <th
+                        onClick={() => SortingData('leaderboard_rank')}
+                        style={{ width: '7%' }}
+                      >
+                        Leaderboard Rank <i className="fas fa-sort"></i>
+                      </th>
+                      <th
+                        onClick={() => SortingData('consistency')}
+                        style={{ width: '5%' }}
+                      >
+                        Consistency <i className="fas fa-sort"></i>
+                      </th>
                     </tr>
                   </thead>
                   <tbody>
@@ -236,21 +340,7 @@ const Dashboard = () => {
                                 )}
                               </td>
                               <td>{datas.attemp_numer}</td>
-                              <td>
-                                {datas.level === 'GENIUS' ? (
-                                  <div className="tagButton Genius">GENIUS</div>
-                                ) : datas.level === 'MASTER' ? (
-                                  <div className="tagButton Master">MASTER</div>
-                                ) : datas.level === 'STARTER' ? (
-                                  <div className="tagButton Starter">
-                                    STARTER
-                                  </div>
-                                ) : (
-                                  <div className="tagButton Challenger">
-                                    CHALLENGER
-                                  </div>
-                                )}
-                              </td>
+                              <td>{datas.level}</td>
                               <td>
                                 {datas.level_attempt_type ? (
                                   <div className="tagButton GoodCss">
@@ -268,92 +358,6 @@ const Dashboard = () => {
                           )
                         })
                       : null}
-
-                    <tr>
-                      <td>
-                        <span>#3</span>
-                      </td>
-                      <td>09-08-2022</td>
-                      <td>Akash Yadav</td>
-                      <td>Add 2 digits</td>
-                      <td>50/60</td>
-                      <td>Best Attempt</td>
-                      <td>3</td>
-                      <td>Genius</td>
-                      <td>Crossed First Time</td>
-                      <td>
-                        <span>#5</span>
-                      </td>
-                      <td>6 days</td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <span>#3</span>
-                      </td>
-                      <td>09-08-2022</td>
-                      <td>Akash Yadav</td>
-                      <td>Add 2 digits</td>
-                      <td>50/60</td>
-                      <td>Best Attempt</td>
-                      <td>3</td>
-                      <td>Genius</td>
-                      <td>Crossed First Time</td>
-                      <td>
-                        <span>#5</span>
-                      </td>
-                      <td>6 days</td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <span>#3</span>
-                      </td>
-                      <td>09-08-2022</td>
-                      <td>Akash Yadav</td>
-                      <td>Add 2 digits</td>
-                      <td>50/60</td>
-                      <td>Best Attempt</td>
-                      <td>3</td>
-                      <td>Genius</td>
-                      <td>Crossed First Time</td>
-                      <td>
-                        <span>#5</span>
-                      </td>
-                      <td>6 days</td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <span>#3</span>
-                      </td>
-                      <td>09-08-2022</td>
-                      <td>Akash Yadav</td>
-                      <td>Add 2 digits</td>
-                      <td>50/60</td>
-                      <td>Best Attempt</td>
-                      <td>3</td>
-                      <td>Genius</td>
-                      <td>Crossed First Time</td>
-                      <td>
-                        <span>#5</span>
-                      </td>
-                      <td>6 days</td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <span>#3</span>
-                      </td>
-                      <td>09-08-2022</td>
-                      <td>Akash Yadav</td>
-                      <td>Add 2 digits</td>
-                      <td>50/60</td>
-                      <td>Best Attempt</td>
-                      <td>3</td>
-                      <td>Genius</td>
-                      <td>Crossed First Time</td>
-                      <td>
-                        <span>#5</span>
-                      </td>
-                      <td>6 days</td>
-                    </tr>
                   </tbody>
                 </table>
               </div>
@@ -375,3 +379,165 @@ const Dashboard = () => {
 }
 
 export default Dashboard
+
+// import React, { useEffect, useState } from 'react'
+// import QuizApi from 'src/api/Quiz'
+// import {
+//   List,
+//   ListItem,
+//   ListItemText,
+//   ListItemIcon,
+//   Collapse,
+//   Badge,
+//   Fab,
+// } from '@material-ui/core'
+// import {
+//   Send,
+//   ChatBubbleOutlineOutlined,
+//   Drafts,
+//   Inbox,
+//   Settings,
+//   OutlinedFlag,
+//   ExpandMore,
+//   ExpandLess,
+//   DeleteForever,
+//   AccountBox,
+//   Score,
+// } from '@material-ui/icons'
+// import Pagination from '@mui/material/Pagination'
+
+// interface ApiInnerData {
+//   activity_name: string
+//   attemp_numer: number
+//   attempt_type: boolean
+//   consistency: string
+//   date: string
+//   leaderboard_rank: number
+//   level: string
+//   level_attempt_type: boolean
+//   name: string
+//   score: number
+// }
+// const Dashboard = () => {
+//   const [open, setOpen] = React.useState(true)
+//   const [opentrash, setOpentrash] = React.useState(true)
+//   const handleClick = (callback: (n: boolean) => void, val: boolean) => {
+//     callback(!val)
+//   }
+//   const [data, setData] = useState<ApiInnerData[]>([])
+//   const [masterdata, setMasterData] = useState<ApiInnerData[]>([])
+
+//   function SortingData(value: string) {
+//     console.log('sorting Data')
+//     if (value === 'date') {
+//       const sortedData = data.sort((x, y) => (x.date < y.date ? -1 : 1))
+//       setData(sortedData)
+//     } else if (value === 'name') {
+//       const sortedData = masterdata[0].sort((x, y) => (x.name < y.name ? -1 : 1))
+//       console.log(sortedData, 'name')
+//       setData([])
+//       setData(sortedData)
+//     } else if (value === 'activity_name') {
+//       const sortedData = data.sort((x, y) =>
+//         x.activity_name < y.activity_name ? -1 : 1
+//       )
+//       setData(sortedData)
+//     } else if (value === 'score') {
+//       const sortedData = data.sort((x, y) => (x.score < y.score ? -1 : 1))
+//       setData(sortedData)
+//     } else if (value === 'attempt_type') {
+//       const sortedData = data.sort((x, y) =>
+//         x.attempt_type < y.attempt_type ? -1 : 1
+//       )
+//       setData(sortedData)
+//     } else if (value === 'attemp_numer') {
+//       const sortedData = data.sort((x, y) =>
+//         x.attemp_numer < y.attemp_numer ? -1 : 1
+//       )
+//       setData(sortedData)
+//     } else if (value === 'level') {
+//       const sortedData: ApiInnerData[] = data.sort((x, y) =>
+//         x.level < y.level ? -1 : 1
+//       )
+//       console.log(sortedData, 'datas')
+//       setData(sortedData)
+//     } else if (value === 'level_attempt_type') {
+//       const sortedData = data.sort((x, y) =>
+//         x.level_attempt_type < y.level_attempt_type ? -1 : 1
+//       )
+//       setData(sortedData)
+//     } else if (value === 'leaderboard_rank') {
+//       const sortedData = data.sort((x, y) =>
+//         x.leaderboard_rank < y.leaderboard_rank ? -1 : 1
+//       )
+//       setData(sortedData)
+//     } else if (value === 'consistency') {
+//       const sortedData = data.sort((x, y) =>
+//         x.consistency < y.consistency ? -1 : 1
+//       )
+//       setData(sortedData)
+//     }
+//   }
+
+//   async function getData() {
+//     const data = ((await QuizApi.tableData()) as unknown) as ApiInnerData[][]
+//     setMasterData(data)
+//     setData(data[0])
+//   }
+//   async function seetData() {
+//     const po = await Promise.all(masterdata[0].sort((x, y) => (x.name < y.name ? -1 : 1)))
+//     setData(po)
+//   }
+//   async function emptyD() {
+//     setData([])
+//   }
+
+//   return (
+//     <>
+//       <button onClick={() => getData()}>get data</button>
+//       <button onClick={() => seetData()}>set data</button>
+//       <button onClick={() => emptyD()}>empty data</button>
+//       {data !== undefined
+//         ? data.map((datas: ApiInnerData, index: number) => {
+//           return (
+//             <tr key={index}>
+//               <td>
+//                 <span>#{index + 1}</span>
+//               </td>
+//               <td>{datas.date}</td>
+//               <td>{datas.name}</td>
+//               <td>{datas.activity_name}</td>
+//               <td>{datas.score}</td>
+//               <td>
+//                 {datas.attempt_type ? (
+//                   <div className="tagButton GoodCss">
+//                     Best Attempt
+//                   </div>
+//                 ) : (
+//                   'Not Best Attempt'
+//                 )}
+//               </td>
+//               <td>{datas.attemp_numer}</td>
+//               <td>{datas.level}</td>
+//               <td>
+//                 {datas.level_attempt_type ? (
+//                   <div className="tagButton GoodCss">
+//                     First Time Crossed
+//                   </div>
+//                 ) : (
+//                   'Already Crossed'
+//                 )}
+//               </td>
+//               <td>
+//                 <span>#{datas.leaderboard_rank}</span>
+//               </td>
+//               <td>{datas.consistency} days</td>
+//             </tr>
+//           )
+//         })
+//         : null}
+//     </>
+//   )
+// }
+
+// export default Dashboard
