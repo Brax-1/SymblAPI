c67f8e96a39794c255ff9a3782e9b41b60d03c0c
Brax-1 <ay701548@gmail.com>
2022-02-06 02:21:56 +0530

    Sorting feature added v1

--- src/components/DashBoard/Dashboard.tsx
+++ src/components/DashBoard/Dashboard.tsx
@@ -36,6 +36,7 @@ interface ApiInnerData {
   name: string
   score: number
 }
+
 const Dashboard = () => {
   const [open, setOpen] = React.useState(true)
   const [opentrash, setOpentrash] = React.useState(true)
@@ -43,54 +44,134 @@ const Dashboard = () => {
     callback(!val)
   }
   const [data, setData] = useState<ApiInnerData[]>([])
+  const [sortStatus, setSortStatus] = useState([
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+  ])
 
   async function SortingData(value: string) {
     let sortedData = data
+    const sortStatusIni = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     if (value === 'date') {
-      sortedData = await Promise.all(
-        data.sort((x, y) => (x.date < y.date ? -1 : 1))
-      )
-    } else if (value === 'name') {
-      sortedData = await Promise.all(
-        data.sort((x, y) => (x.name < y.name ? -1 : 1))
-      )
-    } else if (value === 'activity_name') {
-      sortedData = await Promise.all(
-        data.sort((x, y) => (x.activity_name < y.activity_name ? -1 : 1))
-      )
-    } else if (value === 'score') {
-      sortedData = await Promise.all(
-        data.sort((x, y) => (x.score < y.score ? -1 : 1))
-      )
-    } else if (value === 'attempt_type') {
-      sortedData = await Promise.all(
-        data.sort((x, y) => (x.attempt_type < y.attempt_type ? -1 : 1))
-      )
-    } else if (value === 'attemp_numer') {
-      sortedData = await Promise.all(
-        data.sort((x, y) => (x.attemp_numer < y.attemp_numer ? -1 : 1))
-      )
-    } else if (value === 'level') {
-      sortedData = await Promise.all(
-        data.sort((x, y) => (x.level < y.level ? -1 : 1))
-      )
-    } else if (value === 'level_attempt_type') {
-      sortedData = await Promise.all(
-        data.sort((x, y) =>
-          x.level_attempt_type < y.level_attempt_type ? -1 : 1
+      if (sortStatus[1] === 0) {
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.date < y.date ? -1 : 1))
         )
-      )
+      } else if (sortStatus[1] === 1)
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.date < y.date ? 1 : -1))
+        )
+      sortStatusIni[1] = (sortStatus[1] + 1) % 3
+    } else if (value === 'name') {
+      if (sortStatus[2] === 0) {
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.name < y.name ? -1 : 1))
+        )
+      } else if (sortStatus[2] === 1)
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.name < y.name ? 1 : -1))
+        )
+      sortStatusIni[2] = (sortStatus[2] + 1) % 3
+    } else if (value === 'activity_name') {
+      if (sortStatus[3] === 0) {
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.activity_name < y.activity_name ? -1 : 1))
+        )
+      } else if (sortStatus[3] === 1)
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.activity_name < y.activity_name ? 1 : -1))
+        )
+      sortStatusIni[3] = (sortStatus[3] + 1) % 3
+    } else if (value === 'score') {
+      if (sortStatus[4] === 0) {
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.score < y.score ? -1 : 1))
+        )
+      } else if (sortStatus[4] === 1)
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.score < y.score ? 1 : -1))
+        )
+      sortStatusIni[4] = (sortStatus[4] + 1) % 3
+    } else if (value === 'attempt_type') {
+      if (sortStatus[5] === 0) {
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.attempt_type < y.attempt_type ? -1 : 1))
+        )
+      } else if (sortStatus[5] === 1)
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.attempt_type < y.attempt_type ? 1 : -1))
+        )
+      sortStatusIni[5] = (sortStatus[5] + 1) % 3
+    } else if (value === 'attemp_numer') {
+      if (sortStatus[6] === 0) {
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.attemp_numer < y.attemp_numer ? -1 : 1))
+        )
+      } else if (sortStatus[6] === 1)
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.attemp_numer < y.attemp_numer ? 1 : -1))
+        )
+      sortStatusIni[6] = (sortStatus[6] + 1) % 3
+    } else if (value === 'level') {
+      if (sortStatus[7] === 0) {
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.level < y.level ? -1 : 1))
+        )
+      } else if (sortStatus[7] === 1)
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.level < y.level ? 1 : -1))
+        )
+      sortStatusIni[7] = (sortStatus[7] + 1) % 3
+    } else if (value === 'level_attempt_type') {
+      if (sortStatus[8] === 0) {
+        sortedData = await Promise.all(
+          data.sort((x, y) =>
+            x.level_attempt_type < y.level_attempt_type ? -1 : 1
+          )
+        )
+      } else if (sortStatus[8] === 1)
+        sortedData = await Promise.all(
+          data.sort((x, y) =>
+            x.level_attempt_type < y.level_attempt_type ? 1 : -1
+          )
+        )
+      sortStatusIni[8] = (sortStatus[8] + 1) % 3
     } else if (value === 'leaderboard_rank') {
-      sortedData = await Promise.all(
-        data.sort((x, y) => (x.leaderboard_rank < y.leaderboard_rank ? -1 : 1))
-      )
+      if (sortStatus[9] === 0) {
+        sortedData = await Promise.all(
+          data.sort((x, y) =>
+            x.leaderboard_rank < y.leaderboard_rank ? -1 : 1
+          )
+        )
+      } else if (sortStatus[9] === 1)
+        sortedData = await Promise.all(
+          data.sort((x, y) =>
+            x.leaderboard_rank < y.leaderboard_rank ? 1 : -1
+          )
+        )
+      sortStatusIni[9] = (sortStatus[9] + 1) % 3
     } else if (value === 'consistency') {
-      sortedData = await Promise.all(
-        data.sort((x, y) => (x.consistency < y.consistency ? -1 : 1))
-      )
+      if (sortStatus[10] === 0) {
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.consistency < y.consistency ? -1 : 1))
+        )
+      } else if (sortStatus[10] === 1)
+        sortedData = await Promise.all(
+          data.sort((x, y) => (x.consistency < y.consistency ? 1 : -1))
+        )
+      sortStatusIni[10] = (sortStatus[10] + 1) % 3
     }
-
     setData(sortedData)
+    setSortStatus(sortStatusIni)
   }
 
   async function getData() {
@@ -260,61 +341,131 @@ const Dashboard = () => {
                         onClick={() => SortingData('date')}
                         style={{ width: '7%' }}
                       >
-                        Date <i className="fas fa-sort"></i>
+                        Date{' '}
+                        {sortStatus[1] === 0 ? (
+                          <i className="fas fa-sort"></i>
+                        ) : sortStatus[1] === 1 ? (
+                          <i className="fas fa-sort-up"></i>
+                        ) : (
+                          <i className="fas fa-sort-down"></i>
+                        )}
                       </th>
                       <th
                         onClick={() => SortingData('name')}
                         style={{ width: '13%' }}
                       >
-                        S Name <i className="fas fa-sort"></i>
+                        S Name{' '}
+                        {sortStatus[2] === 0 ? (
+                          <i className="fas fa-sort"></i>
+                        ) : sortStatus[2] === 1 ? (
+                          <i className="fas fa-sort-up"></i>
+                        ) : (
+                          <i className="fas fa-sort-down"></i>
+                        )}
                       </th>
                       <th
                         onClick={() => SortingData('activity_name')}
                         style={{ width: '13%' }}
                       >
-                        Activity Name <i className="fas fa-sort"></i>
+                        Activity Name{' '}
+                        {sortStatus[3] === 0 ? (
+                          <i className="fas fa-sort"></i>
+                        ) : sortStatus[3] === 1 ? (
+                          <i className="fas fa-sort-up"></i>
+                        ) : (
+                          <i className="fas fa-sort-down"></i>
+                        )}
                       </th>
                       <th
                         onClick={() => SortingData('score')}
                         style={{ width: '5%' }}
                       >
-                        Score <i className="fas fa-sort"></i>
+                        Score{' '}
+                        {sortStatus[4] === 0 ? (
+                          <i className="fas fa-sort"></i>
+                        ) : sortStatus[4] === 1 ? (
+                          <i className="fas fa-sort-up"></i>
+                        ) : (
+                          <i className="fas fa-sort-down"></i>
+                        )}
                       </th>
                       <th
                         onClick={() => SortingData('attempt_type')}
                         style={{ width: '7%' }}
                       >
-                        Attempt Type <i className="fas fa-sort"></i>
+                        Attempt Type{' '}
+                        {sortStatus[5] === 0 ? (
+                          <i className="fas fa-sort"></i>
+                        ) : sortStatus[5] === 1 ? (
+                          <i className="fas fa-sort-up"></i>
+                        ) : (
+                          <i className="fas fa-sort-down"></i>
+                        )}
                       </th>
                       <th
                         onClick={() => SortingData('attemp_numer')}
                         style={{ width: '6%' }}
                       >
-                        Attempt Number <i className="fas fa-sort"></i>
+                        Attempt Number{' '}
+                        {sortStatus[6] === 0 ? (
+                          <i className="fas fa-sort"></i>
+                        ) : sortStatus[6] === 1 ? (
+                          <i className="fas fa-sort-up"></i>
+                        ) : (
+                          <i className="fas fa-sort-down"></i>
+                        )}
                       </th>
                       <th
                         onClick={() => SortingData('level')}
                         style={{ width: '6%' }}
                       >
-                        Level <i className="fas fa-sort"></i>
+                        Level{' '}
+                        {sortStatus[7] === 0 ? (
+                          <i className="fas fa-sort"></i>
+                        ) : sortStatus[7] === 1 ? (
+                          <i className="fas fa-sort-up"></i>
+                        ) : (
+                          <i className="fas fa-sort-down"></i>
+                        )}
                       </th>
                       <th
                         onClick={() => SortingData('level_attempt_type')}
                         style={{ width: '9%' }}
                       >
-                        Level Activity <i className="fas fa-sort"></i>
+                        Level Activity{' '}
+                        {sortStatus[8] === 0 ? (
+                          <i className="fas fa-sort"></i>
+                        ) : sortStatus[8] === 1 ? (
+                          <i className="fas fa-sort-up"></i>
+                        ) : (
+                          <i className="fas fa-sort-down"></i>
+                        )}
                       </th>
                       <th
                         onClick={() => SortingData('leaderboard_rank')}
                         style={{ width: '7%' }}
                       >
-                        Leaderboard Rank <i className="fas fa-sort"></i>
+                        Leaderboard Rank{' '}
+                        {sortStatus[9] === 0 ? (
+                          <i className="fas fa-sort"></i>
+                        ) : sortStatus[9] === 1 ? (
+                          <i className="fas fa-sort-up"></i>
+                        ) : (
+                          <i className="fas fa-sort-down"></i>
+                        )}
                       </th>
                       <th
                         onClick={() => SortingData('consistency')}
                         style={{ width: '5%' }}
                       >
-                        Consistency <i className="fas fa-sort"></i>
+                        Consistency{' '}
+                        {sortStatus[10] === 0 ? (
+                          <i className="fas fa-sort"></i>
+                        ) : sortStatus[10] === 1 ? (
+                          <i className="fas fa-sort-up"></i>
+                        ) : (
+                          <i className="fas fa-sort-down"></i>
+                        )}
                       </th>
                     </tr>
                   </thead>
@@ -379,165 +530,3 @@ const Dashboard = () => {
 }
 
 export default Dashboard
-
-// import React, { useEffect, useState } from 'react'
-// import QuizApi from 'src/api/Quiz'
-// import {
-//   List,
-//   ListItem,
-//   ListItemText,
-//   ListItemIcon,
-//   Collapse,
-//   Badge,
-//   Fab,
-// } from '@material-ui/core'
-// import {
-//   Send,
-//   ChatBubbleOutlineOutlined,
-//   Drafts,
-//   Inbox,
-//   Settings,
-//   OutlinedFlag,
-//   ExpandMore,
-//   ExpandLess,
-//   DeleteForever,
-//   AccountBox,
-//   Score,
-// } from '@material-ui/icons'
-// import Pagination from '@mui/material/Pagination'
-
-// interface ApiInnerData {
-//   activity_name: string
-//   attemp_numer: number
-//   attempt_type: boolean
-//   consistency: string
-//   date: string
-//   leaderboard_rank: number
-//   level: string
-//   level_attempt_type: boolean
-//   name: string
-//   score: number
-// }
-// const Dashboard = () => {
-//   const [open, setOpen] = React.useState(true)
-//   const [opentrash, setOpentrash] = React.useState(true)
-//   const handleClick = (callback: (n: boolean) => void, val: boolean) => {
-//     callback(!val)
-//   }
-//   const [data, setData] = useState<ApiInnerData[]>([])
-//   const [masterdata, setMasterData] = useState<ApiInnerData[]>([])
-
-//   function SortingData(value: string) {
-//     console.log('sorting Data')
-//     if (value === 'date') {
-//       const sortedData = data.sort((x, y) => (x.date < y.date ? -1 : 1))
-//       setData(sortedData)
-//     } else if (value === 'name') {
-//       const sortedData = masterdata[0].sort((x, y) => (x.name < y.name ? -1 : 1))
-//       console.log(sortedData, 'name')
-//       setData([])
-//       setData(sortedData)
-//     } else if (value === 'activity_name') {
-//       const sortedData = data.sort((x, y) =>
-//         x.activity_name < y.activity_name ? -1 : 1
-//       )
-//       setData(sortedData)
-//     } else if (value === 'score') {
-//       const sortedData = data.sort((x, y) => (x.score < y.score ? -1 : 1))
-//       setData(sortedData)
-//     } else if (value === 'attempt_type') {
-//       const sortedData = data.sort((x, y) =>
-//         x.attempt_type < y.attempt_type ? -1 : 1
-//       )
-//       setData(sortedData)
-//     } else if (value === 'attemp_numer') {
-//       const sortedData = data.sort((x, y) =>
-//         x.attemp_numer < y.attemp_numer ? -1 : 1
-//       )
-//       setData(sortedData)
-//     } else if (value === 'level') {
-//       const sortedData: ApiInnerData[] = data.sort((x, y) =>
-//         x.level < y.level ? -1 : 1
-//       )
-//       console.log(sortedData, 'datas')
-//       setData(sortedData)
-//     } else if (value === 'level_attempt_type') {
-//       const sortedData = data.sort((x, y) =>
-//         x.level_attempt_type < y.level_attempt_type ? -1 : 1
-//       )
-//       setData(sortedData)
-//     } else if (value === 'leaderboard_rank') {
-//       const sortedData = data.sort((x, y) =>
-//         x.leaderboard_rank < y.leaderboard_rank ? -1 : 1
-//       )
-//       setData(sortedData)
-//     } else if (value === 'consistency') {
-//       const sortedData = data.sort((x, y) =>
-//         x.consistency < y.consistency ? -1 : 1
-//       )
-//       setData(sortedData)
-//     }
-//   }
-
-//   async function getData() {
-//     const data = ((await QuizApi.tableData()) as unknown) as ApiInnerData[][]
-//     setMasterData(data)
-//     setData(data[0])
-//   }
-//   async function seetData() {
-//     const po = await Promise.all(masterdata[0].sort((x, y) => (x.name < y.name ? -1 : 1)))
-//     setData(po)
-//   }
-//   async function emptyD() {
-//     setData([])
-//   }
-
-//   return (
-//     <>
-//       <button onClick={() => getData()}>get data</button>
-//       <button onClick={() => seetData()}>set data</button>
-//       <button onClick={() => emptyD()}>empty data</button>
-//       {data !== undefined
-//         ? data.map((datas: ApiInnerData, index: number) => {
-//           return (
-//             <tr key={index}>
-//               <td>
-//                 <span>#{index + 1}</span>
-//               </td>
-//               <td>{datas.date}</td>
-//               <td>{datas.name}</td>
-//               <td>{datas.activity_name}</td>
-//               <td>{datas.score}</td>
-//               <td>
-//                 {datas.attempt_type ? (
-//                   <div className="tagButton GoodCss">
-//                     Best Attempt
-//                   </div>
-//                 ) : (
-//                   'Not Best Attempt'
-//                 )}
-//               </td>
-//               <td>{datas.attemp_numer}</td>
-//               <td>{datas.level}</td>
-//               <td>
-//                 {datas.level_attempt_type ? (
-//                   <div className="tagButton GoodCss">
-//                     First Time Crossed
-//                   </div>
-//                 ) : (
-//                   'Already Crossed'
-//                 )}
-//               </td>
-//               <td>
-//                 <span>#{datas.leaderboard_rank}</span>
-//               </td>
-//               <td>{datas.consistency} days</td>
-//             </tr>
-//           )
-//         })
-//         : null}
-//     </>
-//   )
-// }
-
-// export default Dashboard
